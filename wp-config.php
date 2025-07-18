fbe6505369a52fa39cd18d3bbd6598422ca5b1c5
btscolombia <83001322+btscolombia@users.noreply.github.com>
2025-07-18 10:18:34 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -3,23 +3,12 @@
  * The base configuration for WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
+// ** Database settings ** //
 define('DB_NAME', $_ENV['WORDPRESS_DB_NAME'] ?: 'railway');
-
-/** Database username */
 define('DB_USER', $_ENV['WORDPRESS_DB_USER'] ?: 'root');
-
-/** Database password */
 define('DB_PASSWORD', $_ENV['WORDPRESS_DB_PASSWORD'] ?: '');
-
-/** Database hostname */
 define('DB_HOST', $_ENV['WORDPRESS_DB_HOST'] ?: 'mysql-68305d35.railway.internal');
-
-/** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
-
-/** The database collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
 /**#@+
@@ -45,18 +34,6 @@ $table_prefix = $_ENV['WORDPRESS_TABLE_PREFIX'] ?: 'wp_';
  * For developers: WordPress debugging mode.
  */
 define('WP_DEBUG', $_ENV['WORDPRESS_DEBUG'] === 'true');
-define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', false);
-
-// Debug: Log database connection info (REMOVE IN PRODUCTION)
-if (WP_DEBUG) {
-    error_log('=== WordPress DB Config Debug ===');
-    error_log('DB_HOST: ' . DB_HOST);
-    error_log('DB_NAME: ' . DB_NAME);
-    error_log('DB_USER: ' . DB_USER);
-    error_log('DB_PASSWORD: ' . (DB_PASSWORD ? 'SET' : 'NOT SET'));
-    error_log('================================');
-}
 
 // Configuración SSL para Cloudflare
 if (isset($_SERVER['HTTP_CF_VISITOR']) && strpos($_SERVER['HTTP_CF_VISITOR'], 'https')) {
@@ -68,4 +45,4 @@ define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
 define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
 
 // Configuración adicional
-define('AUTOMAT
+define
